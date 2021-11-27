fa05bcb9733b2a7d4b78f083ae849acc9efe77de
indywidura <indy.widura16@gmail.com>
2021-11-27 14:47:05 +0700

    GPU_FREQ

--- drivers/misc/mediatek/base/power/mt6785/mtk_gpufreq_core.c
+++ drivers/misc/mediatek/base/power/mt6785/mtk_gpufreq_core.c
@@ -3007,9 +3007,6 @@ static void __mt_gpufreq_setup_opp_table(
 
 	/* setup OPP table by device ID */
 	if (g_segment_id == MT6785T_SEGMENT)
-		g_segment_max_opp_idx = 12;
-		g_segment_max_opp_idx = 0;
-
 		g_segment_max_opp_idx = 3;
 	else if (g_segment_id == MT6785_SEGMENT)
 		g_segment_max_opp_idx = 9;
